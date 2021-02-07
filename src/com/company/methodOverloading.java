da55e113ae88ccacf29a6c7049fabc0940a95240
nsaulakh007 <68895052+nsaulakh007@users.noreply.github.com>
2021-02-07 12:53:54 +0530

    Method overloading and passing arrays as reference in method and printing table using recursion

--- src/com/company/methodOverloading.java
+++ src/com/company/methodOverloading.java
@@ -16,6 +16,18 @@ class Overloading{
         System.out.println(a + " " + b);
     }
 }
+class Recursion{
+    public static void recursion(int num,int srange, int erange){
+        if(srange == (erange+1)){
+            System.out.println("Starting and end range are now same");
+        }else {
+            int ans = num * srange;
+            System.out.println(num + "x" + srange +  "=" + ans);
+            srange = srange + 1;
+            recursion(num,srange,erange);
+        }
+        }
+    }
 public class methodOverloading {
 //    public static void reference(int [] arr){
 //        System.out.println("Value is passed in array through reference");
@@ -27,13 +39,21 @@ public class methodOverloading {
 public static void main(String[] args) {
 //        int [] marks= {23,24,25,26,27};
 //        reference(marks);
+//    Scanner scanner = new Scanner(System.in);
+//    System.out.println("Enter the first string");
+//    String a = scanner.nextLine();
+//    System.out.println("Enter the second string");
+//    String b = scanner.nextLine();
+//    Overloading.MethodOverloading();
+//    Overloading.MethodOverloading(a);
+//    Overloading.MethodOverloading(a, b);
     Scanner scanner = new Scanner(System.in);
-    System.out.println("Enter the first string");
-    String a = scanner.nextLine();
-    System.out.println("Enter the second string");
-    String b = scanner.nextLine();
-    Overloading.MethodOverloading();
-    Overloading.MethodOverloading(a);
-    Overloading.MethodOverloading(a, b);
+    System.out.println("Enter the number of which you want to display the table");
+    int num = scanner.nextInt();
+    System.out.println("Enter the starting range");
+    int srange = scanner.nextInt();
+    System.out.println("Enter the end range");
+    int erange = scanner.nextInt();
+    Recursion.recursion(num,srange,erange);
 }
 }
