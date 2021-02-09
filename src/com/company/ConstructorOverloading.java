3cf26faaab31b90a0274fbaa7d0a30dbbdf41b3a
nsaulakh007 <68895052+nsaulakh007@users.noreply.github.com>
2021-02-09 12:41:21 +0530

    Constructor ovwrloading

---
+++ src/com/company/ConstructorOverloading.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class ConstructorOverloading {
+    ConstructorOverloading(String a){
+        System.out.println("The output constructor with one parameter is: " + a);
+    }
+    ConstructorOverloading(String a, String b){
+        System.out.println("The output of constructor with two parameter is:" + a + "  " + b);
+    }
+    public static void main(String[] args){
+        String a ="Navjot";
+        String b = "Singh";
+        ConstructorOverloading a1 = new ConstructorOverloading(a);
+        ConstructorOverloading a2 = new ConstructorOverloading(a,b);
+
+    }
+}
