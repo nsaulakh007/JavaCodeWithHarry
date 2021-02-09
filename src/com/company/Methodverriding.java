8d09bb583a5cb58342d7ca12e40b8f2fbfd05618
nsaulakh007 <68895052+nsaulakh007@users.noreply.github.com>
2021-02-09 16:11:25 +0530

    Methodverriding

---
+++ src/com/company/Methodverriding.java
@@ -0,0 +1,20 @@
+package com.company;
+class Car{
+    void changeGear(){
+        System.out.println("Car class method call");
+    }
+}
+class SportsCar extends Car{
+    void changeGear(){
+        System.out.println("SportsCar class method call");
+    }
+}
+
+public class Methodverriding extends SportsCar {
+    public static void main(String[] args){
+        Car g1 = new Car();
+        SportsCar g2 = new SportsCar();
+        g1.changeGear();
+        g2.changeGear();
+    }
+}
