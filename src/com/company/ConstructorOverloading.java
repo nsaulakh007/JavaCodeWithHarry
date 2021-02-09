7c1eaecfe1c08e7afcde34d886d589285bf6f5fe
nsaulakh007 <68895052+nsaulakh007@users.noreply.github.com>
2021-02-09 14:37:39 +0530

    Number Guessing

--- src/com/company/ConstructorOverloading.java
+++ src/com/company/ConstructorOverloading.java
@@ -7,11 +7,14 @@ public class ConstructorOverloading {
     ConstructorOverloading(String a, String b){
         System.out.println("The output of constructor with two parameter is:" + a + "  " + b);
     }
+    public void display(){
+        System.out.println("calling function through object");
+    }
     public static void main(String[] args){
         String a ="Navjot";
         String b = "Singh";
         ConstructorOverloading a1 = new ConstructorOverloading(a);
         ConstructorOverloading a2 = new ConstructorOverloading(a,b);
-
+        a1.display();
     }
 }
