49b3f12104a038b4b6dbb6728dcf6feec5e6d6b4
nsaulakh007 <68895052+nsaulakh007@users.noreply.github.com>
2021-02-09 19:23:22 +0530

    super and this keyword uses

--- src/com/company/Methodverriding.java
+++ src/com/company/Methodverriding.java
@@ -1,16 +1,24 @@
 package com.company;
 class Car{
+    int i=4;
     void changeGear(){
+        int i=4;
         System.out.println("Car class method call");
     }
 }
 class SportsCar extends Car{
+    int i=7;
     void change(){
         super.changeGear();
+        System.out.println("This use explained using variable i: " + i);
     }
     void changeGear(){
+        this.i=3;
 
         System.out.println("SportsCar class method call");
+        System.out.println("super class variable fetch before changing: " + super.i);
+        super.i = 5;
+        System.out.println("super class variable fetch after changing: " + super.i);
     }
 }
 
@@ -21,5 +29,6 @@ public class Methodverriding extends SportsCar {
         g1.changeGear();
         g2.change();
         g2.changeGear();
+        g2.change();
     }
 }
