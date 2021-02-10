588c9db9565c4dfbf44464c1b3dd8ae1382768c9
nsaulakh007 <68895052+nsaulakh007@users.noreply.github.com>
2021-02-10 14:23:01 +0530

    Use of static explained

--- src/com/company/Methodverriding.java
+++ src/com/company/Methodverriding.java
@@ -1,12 +1,19 @@
 package com.company;
 class Car{
     int i=4;
+    public Car(int x){
+        System.out.println("Calling Car class constructor and passing value using super keyword " + x);
+    }
     void changeGear(){
         int i=4;
         System.out.println("Car class method call");
     }
 }
 class SportsCar extends Car{
+    public SportsCar(){
+        super(4);
+        System.out.println("Sports car constructor called");
+    }
     int i=7;
     void change(){
         super.changeGear();
@@ -24,7 +31,7 @@ class SportsCar extends Car{
 
 public class Methodverriding extends SportsCar {
     public static void main(String[] args){
-        Car g1 = new Car();
+        Car g1 = new Car(3);
         SportsCar g2 = new SportsCar();
         g1.changeGear();
         g2.change();
